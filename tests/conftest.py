27b6c5078117e08220f3755561688c8a2c02b41f
Antony Polukhin <antoshkka@gmail.com>
2023-01-17 12:23:43 +0300

    up userver

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,36 +5,28 @@ import pytest
 from testsuite.databases.pgsql import discover
 
 
-pytest_plugins = [
-    'pytest_userver.plugins',
-    'testsuite.databases.pgsql.pytest_plugin',
-]
+pytest_plugins = ['pytest_userver.plugins.pgsql']
 
 
 @pytest.fixture(scope='session')
-def root_dir():
+def service_source_dir():
     """Path to root directory service."""
     return pathlib.Path(__file__).parent.parent
 
 
 @pytest.fixture(scope='session')
-def initial_data_path(root_dir):
+def initial_data_path(service_source_dir):
     """Path for find files with data"""
     return [
-        root_dir / 'postgresql/data',
+        service_source_dir / 'postgresql/data',
     ]
 
 
 @pytest.fixture(scope='session')
-def pgsql_local(root_dir, pgsql_local_create):
+def pgsql_local(service_source_dir, pgsql_local_create):
     """Create schemas databases for tests"""
     databases = discover.find_schemas(
         'pg_service_template',  # service name that goes to the DB connection
-        [root_dir.joinpath('postgresql/schemas')],
+        [service_source_dir.joinpath('postgresql/schemas')],
     )
     return pgsql_local_create(list(databases.values()))
-
-
-@pytest.fixture
-def client_deps(pgsql):
-    pass
