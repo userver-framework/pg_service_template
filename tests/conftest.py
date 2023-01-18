fb28c6828160bd5d9bb9b98d00619c18d3695ccb
Antony Polukhin <antoshkka@gmail.com>
2023-01-18 13:25:56 +0300

    the most recent version of userver

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ import pytest
 from testsuite.databases.pgsql import discover
 
 
-pytest_plugins = ['pytest_userver.plugins.pgsql']
+pytest_plugins = ['pytest_userver.plugins.postgresql']
 
 
 @pytest.fixture(scope='session')
