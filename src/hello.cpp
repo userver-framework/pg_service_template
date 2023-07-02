61d3900a9c0970c403eced03499fb65024bd4349
Antony Polukhin <antoshkka@userver.tech>
2023-07-02 11:04:14 +0300

    update userver (#31)

--- src/hello.cpp
+++ src/hello.cpp
@@ -3,6 +3,7 @@
 #include <fmt/format.h>
 
 #include <userver/clients/dns/component.hpp>
+#include <userver/components/component.hpp>
 #include <userver/server/handlers/http_handler_base.hpp>
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/storages/postgres/component.hpp>
